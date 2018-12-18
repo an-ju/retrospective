a31a5d97f23187935ecef932b7f9a86ea346bdc0
an-ju <an_ju@berkeley.edu>
2018-12-18 14:58:28 -0800

    Updated UI for action item input.

--- app/javascript/action_item_form.vue
+++ app/javascript/action_item_form.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="flex max-w-lg">
-        <div class="lg:w-full">
-            <div v-for="item in action_items">
+    <div class="flex-col max-w-lg">
+        <div class="w-full">
+            <div v-for="item in action_items" class="border-2 border-grey-light mx-1 w-full">
                 <p class="text-grey-dark my-2">{{ item.name }}</p>
             </div>
         </div>
-        <form class="w-full" @submit.prevent="submit_form">
-            <div class="lg:flex lg:items-center mb-3">
-                <label class="block text-grey-dark pr-4" for="name">
+        <form class="hover:border-purple-light w-full border-2 border-grey-light mx-1" @submit.prevent="submit_form">
+            <div class="items-center text-lg mb-3 mx-3">
+                <label class="block text-grey-dark pr-4 py-3" for="name">
                     New action item:
                 </label>
-                <input class="border-2 border-grey-dark rounded text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" type="text" id="name" v-model="new_action_item.name"/>
+                <input class="w-full px-2 py-3 border-2 border-grey-dark rounded text-grey-darker focus:outline-none focus:bg-white focus:border-purple" type="text" id="name" v-model="new_action_item.name"/>
             </div>
-            <div class="lg:flex mb-10">
-                <div class="lg:w-full">
+            <div class="mx-3">
+                <div class="lg:w-full my-3">
                     <button class="shadow bg-purple hover:bg-purple-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Add</button>
                 </div>
             </div>
@@ -35,6 +35,7 @@
                 this.$http.post('/action_items', { action_item: this.new_action_item }).then(response => {
                     this.new_action_item.id = response.body.id
                     this.action_items.push(this.new_action_item)
+                    this.new_action_item = {}
                 }, response => {
                     console.log(response)
                 })
